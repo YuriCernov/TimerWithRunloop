2cd0b6ab6e6143e1b599e2da448ba650aac237f7
YuriCernov <jurijs.cernovs.dev@gmail.com>
2025-01-31 12:33:52 +0200

    Added Get, Remove etc.

--- TimerWithRunloop/ContentView.swift
+++ TimerWithRunloop/ContentView.swift
@@ -10,10 +10,34 @@ import SwiftUI
 @Observable
 class ViewModel {
     
+    private let runLoop = RunLoop()
     var time = Date.now
     
+    init() {
+        runThread()
+    }
+    
+    deinit {
+        runLoop.stop()
+    }
+    
+    private func runThread() {
+        Thread.detachNewThread { [weak self] in
+            self?.runLoop.run()
+        }
+    }
+    
     func getDate(after seconds: Double) {
-        time = .now
+        let timer = Timer(timeInterval: seconds, repeats: true) { [weak self] in
+            Foundation.RunLoop.main.perform {
+                self?.time = .now
+            }
+        }
+        runLoop.add(timer: timer)
+    }
+    
+    func stopGetDate() {
+        runLoop.removeTimer()
     }
     
 }
@@ -28,8 +52,12 @@ struct ContentView: View {
             Button("Run", action: {
                 viewModel.getDate(after: 3)
             })
+            Button("Stop", action: {
+                viewModel.stopGetDate()
+            })
         }
     }
+    
 }
 
 class RunLoop {
@@ -37,17 +65,29 @@ class RunLoop {
     typealias Operation = () -> Void
     
     private var customOperations: [Operation] = []
+    private var timer: Timer?
+    
     private let lock = NSLock()
     private let condition = NSCondition()
     private var stopped = false
     
+    
     func run() {
         while !isStopped() {
+            
             condition.wait()
             
             while let operation = getOperation() {
                 operation()
             }
+            
+            if let timer = getTimer() {
+                timer.action()
+                if !timer.repeats {
+                    removeTimer()
+                }
+            }
+            
         }
     }
     
@@ -77,7 +117,33 @@ class RunLoop {
         condition.signal()
     }
     
+    private func getTimer() -> Timer? {
+        lock.lock()
+        defer { lock.unlock() }
+        return timer
+    }
+    
+    func add(timer: Timer) {
+        lock.lock()
+        defer { lock.unlock() }
+        self.timer = timer
+        condition.signal()
+    }
+    
+    func removeTimer() {
+        lock.lock()
+        defer { lock.unlock() }
+        self.timer = nil
+    }
+    
 }
+
+struct Timer {
+    let timeInterval: TimeInterval
+    let repeats: Bool
+    let action: () -> Void
+}
+
 
 #Preview {
     ContentView()
