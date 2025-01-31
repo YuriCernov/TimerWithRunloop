1bfb1ee4d7f5366bc903b0e23b868e79236330fb
YuriCernov <jurijs.cernovs.dev@gmail.com>
2025-01-31 11:53:24 +0200

    Initial Commit

---
+++ TimerWithRunloop/TimerWithRunloopApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerWithRunloopApp.swift
+//  TimerWithRunloop
+//
+//  Created by Yuri Cernov on 31/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TimerWithRunloopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
